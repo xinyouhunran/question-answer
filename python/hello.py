7c630a06b9976a3c2b16c9ad2922df573aba0694
xinyouhunran <2498859749@qq.com>
2018-09-17 17:45:59 +0800

    question

--- python/hello.py
+++ python/hello.py
@@ -3,3 +3,16 @@ if new > 0:
 	print (new);
 else :
 	print (0);
+	
+def fib(max):
+    n, a, b = 0, 0, 1;
+    while n < max:
+        print(b)
+        a, b = b, a + b;
+        n = n + 1;
+    return 'done';
+
+print(fib(6));
+
+
+		
